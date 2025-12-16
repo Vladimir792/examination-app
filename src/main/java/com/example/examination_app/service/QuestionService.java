f3802a801b8ba32d5c7b9548fa41e8683cedd10b
владимир <sitnik792@gmail.ru>
2025-12-16 15:42:45 +0300

    1

--- src/main/java/com/example/examination_app/service/QuestionService.java
+++ src/main/java/com/example/examination_app/service/QuestionService.java
@@ -4,6 +4,7 @@ import com.example.examination_app.model.Question;
 
 import java.util.List;
 
+
 public interface QuestionService<T extends Question> {
     void add(T question);
     boolean remove(String question, String answer);
