b9d67da42159803c1bc4090148705b1aeef09c57
владимир <sitnik792@gmail.ru>
2025-12-17 10:44:53 +0300

    1.2

--- src/main/java/com/example/examination_app/ExaminationAppApplication.java
+++ src/main/java/com/example/examination_app/ExaminationAppApplication.java
@@ -3,6 +3,7 @@ package com.example.examination_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ExaminationAppApplication {
     public static void main(String[] args) {
